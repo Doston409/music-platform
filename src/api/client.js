830845e103fecb9c669a0fc56389216ea7ef052a
Пользователь <dabdugapparov806@gmail.com>
2026-06-20 12:21:56 +0500

    sjnsklndc

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://ezhcelufziyodev.pythonanywhere.com/api/v1'
+const BASE_URL = 'https://github.com/Doston409/music-platform-v2.git'
 
 function getHeaders() {
   const token = localStorage.getItem('access_token')
